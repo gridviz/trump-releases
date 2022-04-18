8d54fc4d513c1910c59d211cb0b5216633008678
Matt Stiles <mattstiles@gmail.com>
2022-04-18 12:54:16 -0700

    latest

--- get_statements.py
+++ get_statements.py
@@ -15,12 +15,15 @@ headers = {
     "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
 }
 
+### Get metadata about the last 10 posts
+
 soups = []
 
 for i in range(0, 10, 10):
     r = requests.get(trump_url + str(i), headers=headers)
     soups.append(BeautifulSoup(r.text, "html.parser"))
-    
+
+### Extract those elements from the pages
 
 dicts = []
 
@@ -35,57 +38,93 @@ for soup in soups:
             "url": str("https://www.donaldjtrump.com") + url,
         }
         dicts.append(data_dict)
-        
-df = pd.DataFrame(dicts).drop_duplicates()
+
+### Dataframe with basic metadata 
+
+df = pd.DataFrame(dicts)
+
 df["date"] = pd.to_datetime(df["date"])
 
+### Get release text for each post's page from our metadata
+
 urls = df["url"].to_list()
+
+### Loop over the url list and extract those release pages
 
 release_soup = []
 
 for url in urls:
     release_page = requests.get(url, headers=headers, timeout=15)
-    release_soup.append(BeautifulSoup(release_page.text, "html.parser"))
-    
+    url_text_dict = {
+        'url': url,
+        'page': BeautifulSoup(release_page.text, "html.parser"),
+    }
+    release_soup.append(url_text_dict)
+
+### Get elements from the release pages
+
 body_text_dicts = []
 
 for release in release_soup:
-    for rl in release.find_all("main", class_="vp-80"):
+    for rl in release['page'].find_all("main", class_="vp-80"):
         try:
-            headline = rl.find("h1", class_="title").text
-            date = rl.find("p", class_="date").text
+            # headline = rl.find("h1", class_="title").text
+            # date = rl.find("p", class_="date").text
             body_text = rl.find("div", class_="body").text
+            url = release['url']
             text_dict = {
-                "headline": headline,
-                "date": date,
+                # "headline": headline,
+                # "date": date,
                 "body_text": body_text,
+                "url": url,
             }
             body_text_dicts.append(text_dict)
         except:
             continue
-            
-text_df = pd.DataFrame(body_text_dicts).drop_duplicates()
+
+### Dataframe with posts and clean up text, date
+
+text_df = pd.DataFrame(body_text_dicts)
 
 text_df["body_text"] = text_df["body_text"].str.replace("\n", "")
-text_df["date"] = pd.to_datetime(text_df["date"])
 
-merged_df = pd.merge(df, text_df, on=["headline", "date"])
+### Merge the metadata and post text into one dataframe
 
-merged_df["year"] = merged_df["date"].dt.year
-merged_df["month"] = merged_df["date"].dt.month_name()
-merged_df["weekday"] = merged_df["date"].dt.day_name()
-merged_df["month_year"] = pd.to_datetime(merged_df["date"]).dt.to_period("M")
+new_df = pd.merge(df, text_df, on=["url"])
 
-archive_df = pd.read_csv('data/processed/all_press_releases_archive.csv')
-latest_df = pd.concat([archive_df, merged_df]).reset_index(drop='True').drop_duplicates()
-latest_df.to_csv(f"data/processed/archives_timeseries/all_press_releases_archive_{today}.csv", index=False)
+### Dates
 
-merged_df.to_csv(f"data/processed/all_press_releases_latest.csv", index=False)
+new_df["year"] = new_df["date"].dt.year
+new_df["month"] = new_df["date"].dt.month_name()
+new_df["weekday"] = new_df["date"].dt.day_name()
+new_df["month_year"] = pd.to_datetime(new_df["date"]).dt.to_period("M")
 
-record_count = len(merged_df) + len(archive_df)
+### Export the most recent 10 posts
 
-if record_count < len(latest_df):
-    diff = len(latest_df) - len(archive_df)
+new_df.to_csv(f"data/processed/all_press_releases_latest.csv", index=False)
+
+### Read the archive 
+
+archive_df = pd.read_csv('data/processed/all_press_releases_archive.csv').drop_duplicates(subset='url')
+
+### Merge 'em together
+
+new_archive_df = pd.concat([archive_df, new_df]).reset_index(drop='True').drop_duplicates(subset='url')
+
+new_archive_df['date'] = pd.to_datetime(new_archive_df['date'])
+
+new_archive_df = new_archive_df.sort_values('date', ascending=False).reset_index(drop='True')
+
+### Export and archive
+new_archive_df.to_csv("data/processed/archives_timeseries/all_press_releases_archive.csv", index=False)
+new_archive_df.to_csv(f"data/processed/archives_timeseries/all_press_releases_archive_{today}.csv", index=False)
+new_archive_df.to_csv(f"data/processed/all_press_releases_archive.csv", index=False)
+
+### Send an email with tailored messages based on whether there's new stuff
+
+diff = (len(new_archive_df) - len(archive_df)) - len(new_df)
+
+if diff >= 1:
     if diff > 1:
         email = f"We've scraped {diff} new items from the former president's news site. See the latest here: https://github.com/gridviz/trump-releases/blob/main/data/processed/all_press_releases_latest.csv"
         subject = f'New Trump scraper result: {diff} new items!'
